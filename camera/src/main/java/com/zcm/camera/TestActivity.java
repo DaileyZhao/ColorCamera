a22d4970d50914e87dd2392f082cca240b3475d2
DavidZhao <1017355712@qq.com>
2018-01-02 17:56:01 +0800

    project build script modify

--- camera/src/main/java/com/zcm/camera/TestActivity.java
+++ camera/src/main/java/com/zcm/camera/TestActivity.java
@@ -3,7 +3,6 @@ package com.zcm.camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 /**
@@ -15,11 +14,5 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new Button(this));
-        new Button(this).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 }
