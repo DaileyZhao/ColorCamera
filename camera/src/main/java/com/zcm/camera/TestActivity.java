7c5020e2422b1a56eb789f3da0499e3d551564bd
DavidZhao <1017355712@qq.com>
2018-01-03 18:03:52 +0800

    project arch modify

--- camera/src/main/java/com/zcm/camera/TestActivity.java
+++ camera/src/main/java/com/zcm/camera/TestActivity.java
@@ -1,9 +1,13 @@
 package com.zcm.camera;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+
+import com.zcm.library.customviews.PieChart;
 
 /**
  * Created by zcm on 2017/12/29.
@@ -13,6 +17,21 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new Button(this));
+        Resources res = getResources();
+
+        setContentView(R.layout.main);
+        final PieChart pie = findViewById(R.id.Pie);
+        pie.addItem("Agamemnon", 2, res.getColor(R.color.seafoam));
+        pie.addItem("Bocephus", 3.5f, res.getColor(R.color.chartreuse));
+        pie.addItem("Calliope", 2.5f, res.getColor(R.color.emerald));
+        pie.addItem("Daedalus", 3, res.getColor(R.color.bluegrass));
+        pie.addItem("Euripides", 1, res.getColor(R.color.turquoise));
+        pie.addItem("Ganymede", 3, res.getColor(R.color.slate));
+
+         findViewById(R.id.Reset).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                pie.setCurrentItem(0);
+            }
+        });
     }
 }
---
+++ library/ui/src/main/java/com/zcm/library/basearch/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.zcm.library.basearch;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zcm on 2018/1/3.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity{
+}
